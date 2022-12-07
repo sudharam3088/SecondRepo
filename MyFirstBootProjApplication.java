fcb0227c6ab723ecfb2c732de22c4be179e64cc3
sudharam3088 <118297435+sudharam3088@users.noreply.github.com>
2022-12-07 13:14:31 -0600

    Add files via upload

---
+++ MyFirstBootProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstBootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstBootProjApplication.class, args);
+	}
+
+}
